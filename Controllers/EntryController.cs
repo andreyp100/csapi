edf903de0a3343a7d22b270dbfc817f7e30c463e
Andrey P <pad@weblock.su>
2025-10-15 11:48:57 +0300

    post put delete method templates added

--- Controllers/EntryController.cs
+++ Controllers/EntryController.cs
@@ -28,5 +28,38 @@ public class EntryController : ControllerBase
     return entry;
   }
 
+  [HttpPost]
+  public IActionResult Create(Entry entry)
+  {
+    EntryService.Add(entry);
+    return CreatedAtAction(nameof(Get), new { id = entry.Id, name = entry.Name, sum = entry.Sum, Date = DateTime.Now }, entry);
+  }
+
+  [HttpPut("{id}")]
+  public IActionResult Update(int id, Entry entry)
+  {
+    if (id != entry.Id)
+      return BadRequest();
+
+    var existingEntry = EntryService.Get(id);
+    if (existingEntry is null)
+      return NotFound();
+
+    EntryService.Update(entry);
+
+    return NoContent();
+  }
+
+  [HttpDelete("{id}")]
+  public IActionResult Delete(int id)
+  {
+    var entry = EntryService.Get(id);
+    if (entry is null)
+      return NotFound();
+
+    EntryService.Delete(id);
+    return NoContent();
+  }
+
 
 }

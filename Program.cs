9e2415b74579fd724503f849e769e6e7cf6dc8fb
Andrey P <pad@weblock.su>
2025-10-17 17:49:45 +0300

    cors allowed

--- Program.cs
+++ Program.cs
@@ -11,12 +11,27 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IEntryService, EntryService>();
 
+const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: MyAllowSpecificOrigins,
+  policy =>
+  {
+    policy.WithOrigins("http://localhost:5174")
+      .AllowAnyHeader()
+      .AllowAnyMethod();
+  });
+});
+
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
